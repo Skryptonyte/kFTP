8727af0c3ed72883e54b3806efa171f7f0d7649d
Rayhan Faizel <rayhan.faizel@gmail.com>
2023-12-25 04:14:33 +0530

    Don't forget the header files :(

--- ftp_serv.h
+++ ftp_serv.h
@@ -14,8 +14,9 @@ void ftpSYST(struct client_context* ctx, struct socket* clientsock);
 void ftpPASV(struct client_context* ctx, struct socket* clientsock);
 void ftpQUIT(struct client_context* ctx, struct socket* clientsock);
 void ftpTYPE(struct client_context* ctx, struct socket* clientsock);
-void ftpRETR(struct client_context* ctx, struct socket* clientsock);
+void ftpRETR(struct client_context* ctx, struct socket* clientsock, const char* path);
 
 int create_passive_data_connection(struct client_context* ctx, int data_port);
 int check_client_auth(struct client_context* ctx);
 int check_dtp(struct client_context* ctx);
+void clean_up_dtp(struct client_context* ctx);
--- ftp_utils.h
+++ ftp_utils.h
@@ -1,7 +1,10 @@
 #include <linux/net.h>
 #include <linux/in.h>
 #include <linux/socket.h>
+#include <linux/fs.h>
 
 int kernel_recv(struct socket* socket, char* buf, int len);
 int kernel_send(struct socket* socket, char* buf, int len);
 unsigned int get_random_open_port(void);
+struct file* kernel_retr_file(char* path);
+ssize_t kernel_retr_read_file(struct file* f, char* buf, int len, loff_t* pos);
